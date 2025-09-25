46cb920fca7251d9cb6304765aeceb084058589f
mara <mara@the-witch.ru>
2025-09-25 19:35:33 +0300

    config file

--- README.md
+++ README.md
@@ -4,10 +4,30 @@ just date selector with `selected date file preview` and editing it in `$EDITOR`
 
 
 ## TODO:
-- [ ] config file
+- [x] config file
 - [ ] rewrite my `notes-alarm` bash script to go as `tty-diary-daemon`
 - [ ] write README
 
+
+## Config file
+
+### Paths
+
+- `$HOME/ttydiary.yaml`
+- `$HOME/.config/ttydiary.yaml`
+
+### Format
+
+```sh
+file-color: "#b1a286"
+file-format: "md"
+diary-dir: "~/Documents/diary"
+monday: true
+```
+
+`file-color`: if file `{diary-dir}/yyyy/mm/dd.{file-format}` is existed and not emtpy (= you have some notes on this date), highlight this date on calendar with this color
+
+`monday: true` for monday as week start or `sunday: true` for sunday
 
 ---
 
--- config/config.go
+++ config/config.go
@@ -5,37 +5,60 @@ import (
 
 	"github.com/maraloon/datepicker"
 	"github.com/spf13/pflag"
+	"github.com/spf13/viper"
 )
 
 type Config struct {
-	NotesColor       string
+	FileColor        string
 	FileFormat       string
 	DiaryDir         string
 	DatepickerConfig datepicker.Config
 }
 
-func ValidateFlags() Config {
-	var notesColor string
+func ValidateFlags() (Config, error) {
+
+	var fileColor string
 	var fileFormat string
 	var diaryDir string
 	var sunday bool
 	var monday bool
-	pflag.StringVar(&notesColor, "color", "6", "Color of dates, which have notes")
+
+	viper.SetConfigName("ttydiary.yaml")
+	viper.SetConfigType("yaml")
+	viper.AddConfigPath(os.Getenv("HOME"))
+	viper.AddConfigPath(os.Getenv("HOME") + "/.config")
+
+	if err := viper.ReadInConfig(); err != nil {
+		if _, ok := err.(viper.ConfigFileNotFoundError); !ok {
+			return Config{}, err
+		}
+	}
+
+	pflag.StringVar(&fileColor, "file-color", "6", "Color of dates, which have notes")
 	pflag.StringVar(&fileFormat, "file-format", "md", "Format of note files")
 	pflag.StringVar(&diaryDir, "diary-dir", os.Getenv("HOME")+"/code/util/notes/diary", "Root dir of notes")
-	pflag.BoolVarP(&sunday, "sunday", "s", true, "Sunday as first day of week")
 	pflag.BoolVarP(&monday, "monday", "m", false, "Monday as first day of week")
+	pflag.BoolVarP(&sunday, "sunday", "s", true, "Sunday as first day of week")
+
 	pflag.Parse()
+	viper.BindPFlags(pflag.CommandLine)
 
 	datepickerConfig := datepicker.DefaultConfig()
-	datepickerConfig.FirstWeekdayIsMo = monday || !sunday
+
+	if pflag.Lookup("monday").Changed { // if -m/--monday
+		datepickerConfig.FirstWeekdayIsMo = viper.GetBool("monday")
+	} else if pflag.Lookup("sunday").Changed { // if -s/--sunday
+		datepickerConfig.FirstWeekdayIsMo = !viper.GetBool("sunday")
+	} else { // get value from config or get default
+		datepickerConfig.FirstWeekdayIsMo = viper.GetBool("monday") || !viper.GetBool("sunday")
+	}
 
 	config := Config{
-		NotesColor:       notesColor,
-		FileFormat:       fileFormat,
-		DiaryDir:         diaryDir,
+		FileColor:        viper.GetString("file-color"),
+		FileFormat:       viper.GetString("file-format"),
+		DiaryDir:         viper.GetString("diary-dir"),
 		DatepickerConfig: datepickerConfig,
 	}
 
-	return config
+	return config, nil
 }
